fa2c959d26ae23b68809e5e7990b5826cbae7c7d
hope <dinesh.0430@gmail.com>
2026-08-18 07:41:37 +0530

    feat(nvim): configure lualine options with auto theme and separators
    Update lualine configuration in lua/plugins/colors.lua to use the options table, set theme to 'auto', and configure clean section and component separators.

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -25,7 +25,11 @@ return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons' },
 	opts = {
-	    theme = 'everforest'
+	    options = {
+		theme = 'auto',
+		section_separators = { left = '', right = '' },
+		component_separators = { left = '|', right = '|' },
+	    },
 	}
     }
 }
